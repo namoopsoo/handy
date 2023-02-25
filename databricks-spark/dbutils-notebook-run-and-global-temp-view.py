cf613a11b60207aa5cd8430a40381502f12f1f7c
michal <namoopsoo>
2023-02-25 15:41:18 -0500

    CRLF to LF

--- databricks-spark/dbutils-notebook-run-and-global-temp-view.py
+++ databricks-spark/dbutils-notebook-run-and-global-temp-view.py
@@ -1,6 +1,15 @@
-# https://docs.databricks.com/notebooks/notebook-workflows.html#pass-structured-data
 
 ### Passing large dataframes with dbutils.notebook.run !
+# At one point when migrating
+# https://docs.databricks.com/notebooks/notebook-workflows.html#pass-structured-data
+
+
+import json
+import pandas as pd
+from uuid import uuid4
+from pyspark.sql import SparkSession
+
+
 def handle_output(raw_output):
     output = json.loads(raw_output)
     dataframes_dict = output.pop("output_dataframe_references", {})
@@ -10,9 +19,9 @@ def handle_output(raw_output):
 
 
 def dereference_dataframes(dataframes_dict):
-    spark = get_spark_session()
+    spark = SparkSession.builder.appName("project").getOrCreate()
     return {
-        name: spark.table(f"global_temp." + view_name)
+        name: spark.table("global_temp." + view_name)
         for (name, view_name) in dataframes_dict.items()
     }
 
@@ -23,7 +32,7 @@ def prepare_dataframe_references(**kwargs):
     Args:
         kwargs: key value pairs of names and dataframes
         e.g.
-        "mbrs_ df": <DataFrames>,
+        "some_df": <DataFrame>,
         "another_df": <DataFrame>,
 
         If any value is not a DataFrame, throws an exception.
@@ -32,13 +41,13 @@ def prepare_dataframe_references(**kwargs):
         Dict mapping the same input names to view names.
         e.g.
         {
-            "mbrs_df": "mbrs_df_fae8f78",
-            "another df": "another_df_0a54d6fe", }
+            "some_df": "some_df_fae8f78",
+            "another_df": "another_df_0a54d6fe", }
     """
     input_dataframes = [
         {"name": k, "df": v, "view_name": f"{k}_{str(uuid4())[:8]}"}
         for (k, v) in kwargs.items()
-        if isinstance(v, DataFrame)
+        if isinstance(v, pd.DataFrame)
     ]
 
     the_rest = {
@@ -58,11 +67,9 @@ def prepare_dataframe_references(**kwargs):
 
 
 def prepare_arguments(**kwargs):
-    """Create the dbutils.notebook. run payload and put dataframes into global_temp."""
-    input_dataframes = {k: v for (k, v) in kwargs.items() if isinstance(v, DataFrame)}
+    """Create the dbutils.notebook.run payload and put dataframes into global_temp."""
+    input_dataframes = {k: v for (k, v) in kwargs.items() if isinstance(v, pd.DataFrame)}
     the_rest = {k: v for (k, v) in kwargs.items() if k not in input_dataframes}
 
     dataframes_dict = prepare_dataframe_references(**input_dataframes)
     return {**the_rest, "input_dataframes": json.dumps(dataframes_dict)}
-
-
